fb250a8f9f7b9ba438244c8d94020ad8f0d2b1e6
nitoc <daren2track@gmail.con>
2023-03-22 04:39:15 +0100

    first commit

---
+++ dist/db/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+let arg = process.env.POSTGRES_PORT || "";
+let db_port = parseInt(arg);
+const pool = new pg_1.Pool({
+    connectionString: "postgres://biedkgtt:i3R1V9z-IQtJdj5RvRWClxyPlRzE7bLg@babar.db.elephantsql.com/biedkgtt"
+});
+exports.default = pool;
